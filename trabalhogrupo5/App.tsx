6a25b8126f49c5d5a0a371b8eedfce5c81cf497a
lucas-xara <lucasxara@gmail.com>
2024-06-20 23:08:11 -0300

    App.tsx up

---
+++ trabalhogrupo5/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Routes } from './src/Routes/';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Routes/>
+    </>
+  );
+}
