b63c9b0b642307d6feb75dc2907a2175aed2d14e
Vinícius Salles <vsmiranda4321@gmail.com>
2024-06-21 16:11:59 -0300

    corrigindo branch - adicionando catalogo

--- trabalhogrupo5/src/Pages/Home/index.tsx
+++ trabalhogrupo5/src/Pages/Home/index.tsx
@@ -1,33 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { View, ScrollView } from "react-native";
-import ProductCard from "../../Components/CardProduto";
-import axios from "axios";
-import styles from "./style";
+import React from "react";
+import { TextInput } from "react-native";
+import { View } from "react-native";
 
-interface Product {
-  id: number;
-  title: string;
-  image: string;
-  price: number;
-}
-
-export const Home: React.FC = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    axios
-      .get("https://6674cee475872d0e0a979434.mockapi.io/produtos")
-      .then((response) => setProducts(response.data))
-      .catch((error) => console.error(error));
-  }, []);
-
+export const Home = () => {
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </View>
-    </ScrollView>
+    <View>
+      <TextInput>Bem vindo a Home</TextInput>
+    </View>
   );
 };
